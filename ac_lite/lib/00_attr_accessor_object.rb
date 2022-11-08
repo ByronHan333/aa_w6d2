1885c54e2a8fb902e999b7dd1305705e9bd22291
ByronHan333 <zhan12@dons.usfca.edu>
2022-11-08 15:44:20 -0800

    add

---
+++ ac_lite/lib/00_attr_accessor_object.rb
@@ -0,0 +1,13 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |name|
+      define_method("#{name}=") do |new_value|
+        instance_variable_set(:"@#{name}", new_value)
+      end
+
+      define_method("#{name}") do |*arg|
+        instance_variable_get(:"@#{name}")
+      end
+    end
+  end
+end
